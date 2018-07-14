b3cff7d7153c4fc4b2e7d2411e6b8a41927e61c4
CAIKE3 <495725770@qq.com>
2018-07-14 14:08:03 +0800

    update WebUtil

--- team_market_common/src/main/java/team/market/common/util/WebUtil.java
+++ team_market_common/src/main/java/team/market/common/util/WebUtil.java
@@ -1,32 +1,32 @@
 package team.market.common.util;
 
-import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
-import java.util.Enumeration;
+import java.util.Map;
 
 public class WebUtil {
 
-    public static <T> T request2Bean(HttpServletRequest request, Class<T> beanClass){
+
+    public static <T> T request2Bean(Map<String,String[]> map, Class<T> beanClass){
         T bean = null;
         try {
             bean = beanClass.newInstance();
             Method[] methods = beanClass.getMethods();
-            Enumeration e = request.getParameterNames();
-            while (e.hasMoreElements()) {
-                String name = (String) e.nextElement();
-                String value = request.getParameter(name);
+
+            for(String name : map.keySet()){
+                String[] values = map.get(name);
                 for (Method method : methods) {
                     if (method.getName().startsWith("set")) {
                         if (("set" + name).toUpperCase().equals(method.getName().toUpperCase())) {
                             String fieldName = beanClass.getDeclaredField(name).getType().getSimpleName();
                             ConvertClazzUtil convertClazz = ConvertClazzUtil.class.newInstance();
                             Method convertMethod = ConvertClazzUtil.class.getDeclaredMethod("convert" + fieldName, String.class);
-                            Object convertValue = convertMethod.invoke(convertClazz, value);
-                            method.invoke(bean, convertValue);
+                            if(values.length > 0){
+                                Object convertValue = convertMethod.invoke(convertClazz, values[values.length - 1]);
+                                method.invoke(bean, convertValue);
+                            }
                         }
                     }
                 }
-
             }
         }catch (Exception e) {
             // TODO Auto-generated catch block
