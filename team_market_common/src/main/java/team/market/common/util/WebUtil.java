4e60d7c29dd3ed7dced804d58678056fc8694518
CAIKE3 <495725770@qq.com>
2018-07-14 14:12:44 +0800

    update WebUtil

--- team_market_common/src/main/java/team/market/common/util/WebUtil.java
+++ team_market_common/src/main/java/team/market/common/util/WebUtil.java
@@ -11,19 +11,16 @@ public class WebUtil {
         try {
             bean = beanClass.newInstance();
             Method[] methods = beanClass.getMethods();
-
             for(String name : map.keySet()){
                 String[] values = map.get(name);
                 for (Method method : methods) {
-                    if (method.getName().startsWith("set")) {
-                        if (("set" + name).toUpperCase().equals(method.getName().toUpperCase())) {
+                    if (method.getName().startsWith("set") && ("set" + name).toUpperCase().equals(method.getName().toUpperCase())) {
+                        if(values != null && values.length > 0){
                             String fieldName = beanClass.getDeclaredField(name).getType().getSimpleName();
                             ConvertClazzUtil convertClazz = ConvertClazzUtil.class.newInstance();
                             Method convertMethod = ConvertClazzUtil.class.getDeclaredMethod("convert" + fieldName, String.class);
-                            if(values.length > 0){
-                                Object convertValue = convertMethod.invoke(convertClazz, values[values.length - 1]);
-                                method.invoke(bean, convertValue);
-                            }
+                            Object convertValue = convertMethod.invoke(convertClazz, values[values.length - 1]);
+                            method.invoke(bean, convertValue);
                         }
                     }
                 }
