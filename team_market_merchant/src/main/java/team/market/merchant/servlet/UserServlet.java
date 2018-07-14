ccb9afad8c6b026b56c93689f951eef2a66a5b59
justin <justin.liu@oocl.com>
2018-07-14 17:17:33 +0800

    update servlet package

---
+++ team_market_merchant/src/main/java/team/market/merchant/servlet/UserServlet.java
@@ -0,0 +1,19 @@
+package team.market.merchant.servlet;
+
+import team.market.common.servlet.BaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends BaseServlet {
+
+    public void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+
+
+    }
+}
