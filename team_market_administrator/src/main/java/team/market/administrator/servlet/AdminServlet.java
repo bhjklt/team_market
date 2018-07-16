4d937b996742975a4bcb26e33dcda37e3f8e92d0
Lance Li <lance.li@oocl.com>
2018-07-16 15:42:49 +0800

    update

--- team_market_administrator/src/main/java/team/market/administrator/servlet/AdminServlet.java
+++ team_market_administrator/src/main/java/team/market/administrator/servlet/AdminServlet.java
@@ -12,50 +12,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AdminServlet extends BaseServlet {
-
+    StoreFormService storeFormService = new StoreFormService();
+    StoreFormRecordService recordService = new StoreFormRecordService();
+    AdFormService adFormService = new AdFormService();
+    StoreService storeService = new StoreService();
+    AdminService adminService = new AdminService();
 
     public String storeRecords(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        StoreFormService storeFormService = new StoreFormService();
+
         List<StoreForm> records = storeFormService.getStoreForms();
-        req.setAttribute("records",records);
+        req.setAttribute("records", records);
         return "/admin/records.jsp";
     }
 
     public String modifyStatus(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String srid = req.getParameter("id");
         String status = req.getParameter("status");
-        StoreFormRecordService recordService = new StoreFormRecordService();
-         recordService.updateStatus(srid, status);
+        recordService.updateStatus(srid, status);
         return storeRecords(req, resp);
     }
 
 
     public String getAllAd(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        AdFormService adFormService = new AdFormService();
-        StoreService storeService = new StoreService();
-        AdminService adminService = new AdminService();
         List<AdStore> adStores = new ArrayList<AdStore>();
         List<AdForm> allAd = adFormService.getAllAd();
-        for (AdForm ad : allAd){
+        for (AdForm ad : allAd) {
             User user = adminService.getUser(ad.getConsumeUId());
             String sid = ad.getsId();
             Store store = storeService.getStoreById(sid);
-            if(store != null && store.getStatus() == Store.AVAILABLE){
-                AdStore adStore = new AdStore(ad,store,user);
+            if (store != null && store.getStatus() == Store.AVAILABLE) {
+                AdStore adStore = new AdStore(ad, store, user);
                 adStores.add(adStore);
             }
         }
         System.out.println(adStores);
-        req.setAttribute("ads",adStores);
+        req.setAttribute("ads", adStores);
         return "/admin/ads.jsp";
     }
 
 
     public String confirmAd(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User admin = (User)req.getSession().getAttribute("admin");
+        User admin = (User) req.getSession().getAttribute("admin");
         String id = req.getParameter("id");
-        AdFormService adFormService = new AdFormService();
-        adFormService.confirmAd(id,admin.getId());
-        return getAllAd(req,resp);
+
+        adFormService.confirmAd(id, admin.getId());
+        return getAllAd(req, resp);
     }
 }
